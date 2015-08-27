1922b454a5c8fbdb1d05acc643fd32f2381a51b6
CodingForAndroid <1078572566@qq.com>
2015-08-27 09:31:02 +0800

    add common utils

--- app/src/main/java/com/bczm/widgetcollections/BaseActivity.java
+++ app/src/main/java/com/bczm/widgetcollections/BaseActivity.java
@@ -5,20 +5,36 @@ import android.os.PersistableBundle;
 import android.support.v4.app.FragmentActivity;
 
 /**
- * @author：Jorge on 2015/8/26 18:03
+ * @author Jorge on 2015/8/26 18:03
  */
 public abstract  class BaseActivity extends FragmentActivity{
-
+    /** 记录处于前台的Activity */
+    private static BaseActivity mForegroundActivity = null;
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
-          createContent();
-          initViews();
-          setListeners();
-          free();
+        createContent();
+        initViews();
+        setListeners();
+        free();
     }
 
+    @Override
+    protected void onResume() {
+        mForegroundActivity = this;
+        super.onResume();
+    }
 
+    @Override
+    protected void onPause() {
+        mForegroundActivity = null;
+        super.onPause();
+    }
+
+    /** 获取当前处于前台的activity */
+    public static BaseActivity getForegroundActivity() {
+        return mForegroundActivity;
+    }
     /**
      * setContentView(int layoutResID)
      */
