abcde8df10ef222e8338505af797c1a0bfe770ae
CodingForAndroid <1078572566@qq.com>
2015-08-28 09:43:43 +0800

    add  inject plugin

--- app/src/main/java/com/bczm/widgetcollections/ui/BaseActivity.java
+++ app/src/main/java/com/bczm/widgetcollections/ui/BaseActivity.java
@@ -1,4 +1,4 @@
-package com.bczm.widgetcollections;
+package com.bczm.widgetcollections.ui;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
--- app/src/main/java/com/bczm/widgetcollections/ui/MainActivity.java
+++ app/src/main/java/com/bczm/widgetcollections/ui/MainActivity.java
@@ -1,9 +1,11 @@
-package com.bczm.widgetcollections;
+package com.bczm.widgetcollections.ui;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.bczm.widgetcollections.R;
 
 
 public class MainActivity extends ActionBarActivity {
